14c878abfb857c0d91f1bc161724125a74a29ead
lngakoi <lnakhoi1811@gmail.com>
2025-11-28 23:24:24 +0700

    Implement login UI

--- v-dominant-frontend/src/app/app.ts
+++ v-dominant-frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoginComponent} from './features/auth/login.component/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- v-dominant-frontend/src/app/features/auth/login.component/login.component.ts
+++ v-dominant-frontend/src/app/features/auth/login.component/login.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-login.component',
-  imports: [],
+  selector: 'app-login',
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+        email: ['', Validators.required, Validators.email],
+        password: ['', Validators.required, Validators.min(6)]
+      }
+    )
+  }
+
+  onSubmit() {
+
+  }
 }
