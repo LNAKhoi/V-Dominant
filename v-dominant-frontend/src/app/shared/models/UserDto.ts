025d495d138a50fc2c257675e13f3ec23fa7786e
lngakoi <lnakhoi1811@gmail.com>
2025-12-06 16:21:10 +0700

    Add LBD template

--- v-dominant-frontend/src/app/shared/models/UserDto.ts
+++
@@ -1,7 +0,0 @@
-export interface UserDto {
-  firstName?: string;
-  lastName?: string;
-  password: string;
-  email: string;
-  address?: string;
-}
--- v-dominant-frontend/src/main.ts
+++ v-dominant-frontend/src/main.ts
@@ -1,6 +1,26 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+/*!
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+ =========================================================
+ * Light Bootstrap Dashboard Angular - v1.9.0
+ =========================================================
+
+ * Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-angular2
+ * Copyright 2020 Creative Tim (http://www.creative-tim.com)
+ * Licensed under MIT
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ */
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
